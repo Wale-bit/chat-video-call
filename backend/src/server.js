442715d543bdf6c1fe0a41721c3033dc1dedb01d
Wale Eniola <eniolaolawale2017@gmail.com>
2025-09-02 20:50:22 +0100

    feat: configure project for vercel deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,13 +11,12 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
@@ -29,15 +28,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+export default app;
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
-});
