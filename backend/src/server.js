1640191a41921812e445879e7923b28250e8059b
Wale Eniola <eniolaolawale2017@gmail.com>
2025-09-02 22:01:19 +0100

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,12 +11,13 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+const PORT = process.env.PORT;
 
-connectDB();
+const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
@@ -28,5 +29,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-export default app;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
