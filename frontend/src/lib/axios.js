0ed697c71a6f8be3f8b62cf2bbc9172a17200a8d
Wale Eniola <eniolaolawale2017@gmail.com>
2025-09-03 16:53:53 +0100

    jusuy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://videocall-backend-a5c5d0i9e-eniola-olawales-projects.vercel.app/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
